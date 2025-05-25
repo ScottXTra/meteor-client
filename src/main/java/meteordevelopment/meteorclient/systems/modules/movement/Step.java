4818e095a87571f62de25bec9f49c297653b7ae3
ScottXTra <39224367+ScottXTra@users.noreply.github.com>
2025-05-24 20:08:14 -0400

    Add optional boat step

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Step.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Step.java
@@ -12,10 +12,22 @@ import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.entity.DamageUtils;
+import meteordevelopment.meteorclient.utils.player.FindItemResult;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.ChestBoatEntity;
+import net.minecraft.item.BoatItem;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.OptionalDouble;
 
@@ -54,6 +66,16 @@ public class Step extends Module {
         .build()
     );
 
+    private final Setting<Boolean> boatStep = sgGeneral.add(new BoolSetting.Builder()
+        .name("boat-step")
+        .description("Automatically places a boat before stepping and breaks it after stepping.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private Entity placedBoat;
+    private int boatTimer;
+
     private float prevStepHeight;
     private boolean prevPathManagerStep;
 
@@ -67,6 +89,9 @@ public class Step extends Module {
 
         prevPathManagerStep = PathManagers.get().getSettings().getStep().get();
         PathManagers.get().getSettings().getStep().set(true);
+
+        placedBoat = null;
+        boatTimer = 0;
     }
 
     @EventHandler
@@ -75,6 +100,7 @@ public class Step extends Module {
         mc.player.setBoundingBox(mc.player.getBoundingBox().offset(0, 1, 0));
         if (work && (!safeStep.get() || (getHealth() > stepHealth.get() && getHealth() - getExplosionDamage() > stepHealth.get()))){
             mc.player.getAttributeInstance(EntityAttributes.STEP_HEIGHT).setBaseValue(height.get());
+            if (boatStep.get()) handleBoatStep();
         } else {
             mc.player.getAttributeInstance(EntityAttributes.STEP_HEIGHT).setBaseValue(prevStepHeight);
         }
@@ -86,6 +112,8 @@ public class Step extends Module {
         mc.player.getAttributeInstance(EntityAttributes.STEP_HEIGHT).setBaseValue(prevStepHeight);
 
         PathManagers.get().getSettings().getStep().set(prevPathManagerStep);
+
+        placedBoat = null;
     }
 
     private float getHealth() {
@@ -99,6 +127,50 @@ public class Step extends Module {
                 .mapToDouble(entity -> DamageUtils.crystalDamage(mc.player, entity.getPos()))
                 .max();
         return crystalDamage.orElse(0.0);
+    }
+
+    private void handleBoatStep() {
+        if (placedBoat != null) {
+            boatTimer++;
+
+            if (!placedBoat.isAlive()) {
+                placedBoat = null;
+                return;
+            }
+
+            if (boatTimer > 1) {
+                mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(placedBoat, mc.player.isSneaking()));
+                mc.getNetworkHandler().sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+                placedBoat = null;
+            }
+
+            return;
+        }
+
+        Direction dir = mc.player.getHorizontalFacing();
+        BlockPos pos = mc.player.getBlockPos().offset(dir);
+        if (!mc.world.getBlockState(pos).isAir() && mc.world.getBlockState(pos.up()).isAir()) {
+            FindItemResult boat = InvUtils.findInHotbar(stack -> stack.getItem() instanceof BoatItem);
+            if (boat.found()) {
+                Vec3d hitPos = Vec3d.ofCenter(pos.up());
+                Rotations.rotate(Rotations.getYaw(hitPos), Rotations.getPitch(hitPos), () -> {
+                    if (boat.getHand() != null) mc.interactionManager.interactItem(mc.player, boat.getHand());
+                    else {
+                        InvUtils.swap(boat.slot(), true);
+                        mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+                        InvUtils.swapBack();
+                    }
+                });
+
+                for (Entity entity : mc.world.getEntities()) {
+                    if ((entity instanceof BoatEntity || entity instanceof ChestBoatEntity) && entity.squaredDistanceTo(hitPos) < 4) {
+                        placedBoat = entity;
+                        boatTimer = 0;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     public enum ActiveWhen {
