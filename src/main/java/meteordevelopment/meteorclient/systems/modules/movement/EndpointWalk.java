9d633f6482acb321b25c3a841c6d9fa3c966ae17
Scott Weselan <mpghloginmpgh@gmail.com>
2025-09-01 20:41:47 -0400

    added visualization to the endpoint walk

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/EndpointWalk.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/EndpointWalk.java
@@ -6,17 +6,22 @@
 package meteordevelopment.meteorclient.systems.modules.movement;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.utils.player.Rotations;
+import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.Queue;
@@ -44,6 +49,7 @@ public class EndpointWalk extends Module {
     private double x1, x2;
     private boolean toFirst;
     private boolean spoofing;
+    private Vec3d serverPos = Vec3d.ZERO;
 
     private final Queue<QueuedPacket> queue = new ConcurrentLinkedQueue<>();
 
@@ -58,6 +64,7 @@ public class EndpointWalk extends Module {
         x2 = x - length.get();
         toFirst = true;
         spoofing = false;
+        serverPos = mc.player.getPos();
     }
 
     @Override
@@ -89,6 +96,8 @@ public class EndpointWalk extends Module {
             flushQueue();
             spoofing = false;
         }
+
+        if (!spoofing) serverPos = mc.player.getPos();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST + 50)
@@ -103,6 +112,37 @@ public class EndpointWalk extends Module {
         QueuedPacket p;
         while ((p = queue.poll()) != null) {
             p.connection.send(p.packet, null, true);
+        }
+    }
+
+    @EventHandler
+    private void onRender(Render3DEvent event) {
+        if (mc.player == null || mc.world == null) return;
+
+        double r = radius.get();
+        for (PlayerEntity p : mc.world.getPlayers()) {
+            renderCircle(event, p.getX(), p.getY(), p.getZ(), r, Color.RED);
+        }
+
+        Box box = mc.player.getBoundingBox().offset(
+            serverPos.x - mc.player.getX(),
+            serverPos.y - mc.player.getY(),
+            serverPos.z - mc.player.getZ()
+        );
+
+        event.renderer.box(box, Color.RED, Color.RED, ShapeMode.Lines, 0);
+    }
+
+    private void renderCircle(Render3DEvent event, double x, double y, double z, double r, Color color) {
+        double prevX = x + r;
+        double prevZ = z;
+        for (int i = 1; i <= 40; i++) {
+            double angle = Math.PI * 2 * i / 40;
+            double nextX = x + Math.cos(angle) * r;
+            double nextZ = z + Math.sin(angle) * r;
+            event.renderer.line(prevX, y, prevZ, nextX, y, nextZ, color);
+            prevX = nextX;
+            prevZ = nextZ;
         }
     }
 
